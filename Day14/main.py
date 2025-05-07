14efb67008fef70764ac9a63b5624d1af21b408f
molalign8468 <molaligngetahun229@gmail.com>
2025-05-07 17:59:01 +0300

    Higher Lower Game

---
+++ Day14/main.py
@@ -0,0 +1,40 @@
+import random
+from data import data
+import art
+
+opponent_A = random.choice(data)
+opponent_B = random.choice(data)
+
+def who_has_bigger(A,B):
+    if A > B:
+      return "a"
+    elif A< B:
+       return "b"
+result = who_has_bigger(opponent_A["follower_count"],opponent_B["follower_count"])
+
+score = 0
+isGame_over = False
+
+while not isGame_over:
+   print(art.logo)
+   print(f"Compare A: {opponent_A["name"]}, a {opponent_A["description"]}, from {opponent_A["country"]}.")
+   print(art.vs)
+   print(f"Compare B: {opponent_B["name"]}, a {opponent_B["description"]}, from {opponent_B["country"]}.")
+   user_in = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+   if user_in == result :
+     score+=1
+     print(f"Correct! Current score: {score}\n")
+     opponent_A = opponent_B
+     opponent_B = random.choice(data)
+    
+     result = who_has_bigger(opponent_A["follower_count"],opponent_B["follower_count"])
+
+   else:
+      isGame_over = True
+      print(f"\nSorry, that's wrong. Final score: {score}")
+      
+print("Game Over!")
+
+
+
