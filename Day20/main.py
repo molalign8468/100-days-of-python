12972a84062387e76cd6d5ec48f14fc746f34153
molalign8468 <molaligngetahun229@gmail.com>
2025-07-18 03:37:40 +0300

    Snake Game Updated

--- Day20/main.py
+++ Day20/main.py
@@ -33,8 +33,8 @@ while game_is_on:
 
     snake.move() 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
-        game_is_on = False
-        scoreBoard.game_over()
+        scoreBoard.reset()
+        snake.reset()
 
     if snake.head.distance(food) < 15:
         print("Collision!")
@@ -44,8 +44,8 @@ while game_is_on:
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreBoard.game_over()
+            scoreBoard.reset()
+            snake.reset()
         
 
 screen.exitonclick()
--- Day20/scorebord.py
+++ Day20/scorebord.py
@@ -7,19 +7,23 @@ FONT = ("Arial", 24, "normal")
 class ScoreBoard(Turtle): 
     def __init__(self):
         super().__init__() 
-        self.score = 0     
+        self.score = 0   
+        self.high_score=0  
         self.goto(0, 250)  
         self.color("white") 
         self.hideturtle() 
         self.penup()      
         self.refresh()    
     def refresh(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High score {self.high_score}", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score=0
+        self.refresh()
 
     def inc_score(self):
         self.score += 1  
-        self.clear()      
         self.refresh()    
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game over ")
+  
