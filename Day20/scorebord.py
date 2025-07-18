3e17c476b56fb94bb6216fceabdaee08a671965c
molalign8468 <molaligngetahun229@gmail.com>
2025-07-18 04:16:57 +0300

    Snake High Score         persistence

--- Day20/scorebord.py
+++ Day20/scorebord.py
@@ -8,12 +8,15 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__() 
         self.score = 0   
-        self.high_score=0  
+        self.high_score = 0
+
+        with open("high_score.txt") as data:
+            self.high_score = int(data.read())
         self.goto(0, 250)  
         self.color("white") 
         self.hideturtle() 
         self.penup()      
-        self.refresh()    
+        self.refresh() 
     def refresh(self):
         self.clear()
         self.write(f"Score: {self.score} High score {self.high_score}", align=ALIGNMENT, font=FONT)
@@ -21,6 +24,8 @@ class ScoreBoard(Turtle):
         if self.score > self.high_score:
             self.high_score = self.score
         self.score=0
+        with open("high_score.txt",mode="w") as highScore:
+            highScore.write(str(self.high_score))
         self.refresh()
 
     def inc_score(self):
