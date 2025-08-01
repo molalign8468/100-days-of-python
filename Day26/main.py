8b67262e65445c492d1894c8c72ab201322f2576
molalign8468 <molaligngetahun229@gmail.com>
2025-08-01 17:06:37 +0300

    phonetic_alphabet project Update

--- Day26/main.py
+++ Day26/main.py
@@ -3,7 +3,17 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 new_list = {row.letter: row.code for (index, row) in data.iterrows()}
 
-user_input = input("Enter a Word: ").upper()
-letter_characters = [char for char in user_input]
-phonetic_codes = [new_list[char] for char in letter_characters]
-print(phonetic_codes)
+
+has_finished = True
+while has_finished:
+    user_input = input("Enter a Word: ").upper()
+    letter_characters = [char for char in user_input]
+    try: 
+        phonetic_codes = [new_list[char] for char in letter_characters]
+    except KeyError as error_msg:
+        print("sorry, only letters in the alphabet please")
+    else:
+        print(phonetic_codes)
+        has_finished=False
+
+
