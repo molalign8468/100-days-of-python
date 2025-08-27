51bebb85ed35526ce0eaf7cca2eb6d176d584a89
molalign8468 <molaligngetahun229@gmail.com>
2025-08-27 16:41:42 +0300

    Add basic Flask app and timer decorator example

---
+++ Day54/main.py
@@ -0,0 +1,25 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} took {end - start:.4f} seconds")
+        return result
+    return wrapper
+
+@timer
+def slow_function():
+    time.sleep(2)
+    print("Done!")
+
+
+@timer
+def fast_function():
+    time.sleep(5)
+    print("Done!")
+
+
+slow_function()
+fast_function()
